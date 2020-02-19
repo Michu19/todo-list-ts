79802f89b02d1eff7499b0eff2cece43673fd9c6
Michał Szymański <michal.szymanski@softable.pl>
2020-02-19 15:52:25 +0100

    feat(client): create-react-app and added mainpage and theme context

---
+++ src/pages/App.tsx
@@ -0,0 +1,21 @@
+import React, {Suspense} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import Loader from '../components/Loader';
+import MainPage from './MainPage/MainPage';
+
+
+
+
+const App = () => {   
+    return ( 
+        <Suspense fallback={<Loader/>}>
+            <Router>
+                <div>
+                    <Route exact path="/" component={MainPage}/>                    
+                </div>
+            </Router>
+        </Suspense>       
+    );
+};
+
+export default App;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+
+
+const MainPage = () => {
+  return <div>Mainpage</div>;
+};
+
+export default MainPage;
